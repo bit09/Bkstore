f2977223188b5fd5ad92afffe486afa841cc7c57
bit09 <egidijus.kunigonis@gmail.com>
2018-11-16 11:46:25 +0200

    Add project files.

---
+++ Bkstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bkstore.Startup))]
+namespace Bkstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
